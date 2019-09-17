48d160abe277bbf9eef2f0ef046e77884334c427
Modurotolu Olokode <durolawk@gmail.com>
2019-09-17 19:30:24 +0100

    added save functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import MovieCard from './MovieCard';
 
 const Movie = (props) => {
+  const {onclick} = props;
   const [movie, setMovie] = useState(null);
   const id = props.match.params.id;
   
@@ -23,10 +24,11 @@ const Movie = (props) => {
   }, [id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    // const addToSavedList = props.addToSavedList;
+    //addToSavedList(movie)
+    onclick(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -36,7 +38,7 @@ const Movie = (props) => {
   return (
     <div className="save-wrapper">
       <MovieCard title={title} director={director} metascore={metascore} stars={stars} />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
